a9e80128a4e96973380e77889479e9a403b246f2
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-19 18:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-document.getElementById("submit").addEventListener("click", ()=>{
+document.getElementById("submit").addEventListener("click", (event)=>{
+	
+	event.preventDefault();
 	let username= document.getElementById("username").value;
 	let password= document.getElementById("password").value;
 
 	
-	if(document.getElementById("checkbox").value){
+	if(document.getElementById("checkbox").checked){
 		localStorage.setItem("Username", username);
 		localStorage.setItem("Password", password);
-		alert("Logged in as");
-		
+		alert("Logged in as " + username);
+		document.getElementById("myform").reset();
+	}else{
+		localStorage.removeItem("Username");
+        localStorage.removeItem("Password");
 	}
 })
+
+
+document.getElementById("existing").addEventListener("click", (event) => {
+   
+    event.preventDefault();
+
+	let saveduser= localStorage.getItem("Username");
+	if(saveduser){
+		 alert("Logged in as " + saveduser);
+	
+	}else{
+		alert("No saved user found");
+	}
+});
+
+
+
+
+
+
+
+
+
+
+

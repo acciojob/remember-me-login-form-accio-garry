d50d639823477d7cf1ce971aee0657fd64e835ac
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-19 18:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener("click", ()=>{
+	let username= document.getElementById("username").value;
+	let password= document.getElementById("password").value;
+
+	
+	if(document.getElementById("checkbox").value){
+		localStorage.setItem("Username", username);
+		localStorage.setItem("Password", password);
+		alert("Logged in as");
+		
+	}
+})
